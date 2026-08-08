3527cba41587b8c367cef0220344efe2db38b4d2
shaikhshakib <shakibshaikh2607@gmail.com>
2026-08-08 22:30:36 +0530

    feat: add interview session tracking and answer storage

--- app/main.py
+++ app/main.py
@@ -4,10 +4,12 @@ from fastapi.templating import Jinja2Templates
 from app.data.questions import QUESTIONS
 from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.responses import JSONResponse
+import uuid 
 
 
 app = FastAPI(title="AI Interview Assistant")
 
+interviews = {}
 app.mount(
     "/static",
     StaticFiles(directory="app/static"),
@@ -34,10 +36,14 @@ async def interview_page(
     topic: str
 ):
 
+    interview_id = str(uuid.uuid4())
+
     questions = QUESTIONS.get(topic.lower())
 
     if not questions:
         return HTMLResponse("Invalid Interview Topic")
+
+    interviews[interview_id] = []
 
     return templates.TemplateResponse(
         request=request,
@@ -46,17 +52,31 @@ async def interview_page(
             "topic": topic.capitalize(),
             "question": questions[0],
             "question_number": 1,
-            "total_questions": len(questions)
+            "total_questions": len(questions),
+            "interview_id": interview_id
         }
     )
 @app.post("/next-question")
 async def next_question(data: dict):
 
     topic = data.get("topic").lower()
-
     current = data.get("current")
+    answer = data.get("answer")
 
     questions = QUESTIONS.get(topic)
+
+    if not questions:
+        return JSONResponse({
+            "error": "Invalid topic"
+        })
+
+    interview_id = data.get("interview_id")
+
+    interviews[interview_id].append({
+        "question_number": current,
+        "question": questions[current - 1],
+        "answer": answer
+    })
 
     if current >= len(questions):
 
@@ -65,11 +85,7 @@ async def next_question(data: dict):
         })
 
     return JSONResponse({
-
         "finished": False,
-
         "question": questions[current],
-
         "question_number": current + 1
-
     })
